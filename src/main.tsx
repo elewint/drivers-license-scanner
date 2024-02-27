2d223f0af6f716a8081ec4eda6a1ef1f55176230
elewint <elewint@gmail.com>
2024-02-27 15:01:39 -0800

    Fix casing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./app.tsx";
 import "./index.css";
 import Footer from "./components/common/footer.tsx";
 import { Toaster } from "react-hot-toast";
