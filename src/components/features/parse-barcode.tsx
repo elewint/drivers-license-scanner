894aa193467527d7862cd326533b7e5e70c811f0
elewint <elewint@gmail.com>
2024-02-27 18:30:47 -0800

    Re-add functionality

--- src/components/features/parse-barcode.tsx
+++ src/components/features/parse-barcode.tsx
@@ -5,45 +5,42 @@ type ParseBarcodeProps = {
 };
 
 export default function ParseBarcode({ barcodeData }: ParseBarcodeProps) {
-  const license_data = aamva.pdf417(barcodeData);
+  const license_data = aamva?.pdf417(barcodeData);
 
-  const testFunction = () => {
-    console.log(license_data);
+  const titleCase = (str: string) => {
+    return str
+      .toLowerCase()
+      .replace(/(?:^|\s)\w/g, (match) => match.toUpperCase());
   };
-  // const titleCase = (str: string) => {
-  //   return str
-  //     .toLowerCase()
-  //     .replace(/(?:^|\s)\w/g, (match) => match.toUpperCase());
-  // };
 
-  // const date = (date: string) => {
-  //   return `${date.slice(4, 6)}/${date.slice(6, 8)}/${date.slice(0, 4)}`;
-  // };
+  const date = (date: string) => {
+    return `${date.slice(4, 6)}/${date.slice(6, 8)}/${date.slice(0, 4)}`;
+  };
 
-  // const getFullName = () => {
-  //   return `${titleCase(license_data.name.first)} ${titleCase(
-  //     license_data.name.middle
-  //   )} ${titleCase(license_data.name.last)}`;
-  // };
+  const getFullName = () => {
+    return `${titleCase(license_data.name.first)} ${titleCase(
+      license_data.name.middle
+    )} ${titleCase(license_data.name.last)}`;
+  };
 
-  // const getAddress = () => {
-  //   return titleCase(license_data.address);
-  // };
+  const getAddress = () => {
+    return titleCase(license_data.address);
+  };
 
-  // const getExpirationDate = () => {
-  //   return `${date(license_data.expiration_date)}`;
-  // };
+  const getExpirationDate = () => {
+    return `${date(license_data.expiration_date)}`;
+  };
 
-  // const getIDNumber = () => {
-  //   return `${license_data.id}`;
-  // };
+  const getIDNumber = () => {
+    return `${license_data.id}`;
+  };
 
   return (
-    <div onLoad={testFunction}>
-      {/* <h3>Full name: {getFullName()}</h3>
+    <div>
+      <h3>Full name: {getFullName()}</h3>
       <h3>Address: {getAddress()}</h3>
       <h3>Expiration date: {getExpirationDate()}</h3>
-      <h3>ID Number: {getIDNumber()}</h3> */}
+      <h3>ID Number: {getIDNumber()}</h3>
     </div>
   );
 }
