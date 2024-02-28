c63a5d89c70ea8e290d9da039f3504e5d96fd222
elewint <elewint@gmail.com>
2024-02-27 20:54:26 -0800

    Change design and add typescript logo

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,18 +1,22 @@
 import "./footer.css";
 import reactLogo from "/src/assets/react.svg";
+import typeScriptLogo from "/src/assets/typescript.svg";
 import viteLogo from "/src/assets/vite.svg";
 
 const Footer = () => {
   return (
     <div className="footer-parent">
       <div className="footer">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        <p>Made by Eli Intriligator with React, TypeScript, and Vite.</p>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <p>Made with Vite and React.</p>
+        <a href="https://www.typescriptlang.org/" target="_blank">
+          <img src={typeScriptLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
       </div>
     </div>
   );
